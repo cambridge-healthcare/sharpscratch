347cd7c21955bf0801566eaa19a9b200c4cee4bc
Szymon Witamborski <szywon@live.com>
2013-01-26 23:01:43 +0000

    New procedure creation (stub, only title at this point).

---
+++ public/new_procedure.js
@@ -0,0 +1,46 @@
+angular.module('SharpScratch', ['ngResource']);
+
+function NewProcedure ($scope, $resource) {
+  $scope.show = true;
+
+  $scope.steps         = [''];
+  $scope.complications = [];
+
+  $scope.inc = function (list) {
+    list.push('');
+  };
+
+  function get_values (title_class, text_class) {
+    var things = [];
+
+    var titles = document.querySelectorAll(title_class);
+    var texts = document.querySelectorAll(text_class);
+
+    for (var i = 0; i < titles.length; ++i) {
+      things.push({
+        title: titles[i].value,
+        text:  texts[i].value
+      });
+    }
+
+    return things;
+  }
+
+  var saver = $resource('/procedures.json');
+
+  $scope.save = function () {
+    var steps = get_values('.step_title', '.step_text');
+    var complications = get_values('.complication_title',
+      '.complication_text');
+
+    saver.save({
+      title: $scope.title
+    }, function () {
+      console.log(arguments);
+    });
+
+    console.log(steps);
+    console.log(complications);
+    console.log($scope);
+  };
+}
