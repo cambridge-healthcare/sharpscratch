7c868d7794f25a6344c09696095af5d0401fb66d
Gerhard Lazu <gerhard@lazu.co.uk>
2013-01-26 21:23:15 +0000

    Main API endpoints for procedures

--- lib/api.rb
+++ lib/api.rb
@@ -1,11 +1,8 @@
 require 'grape'
 require 'grape-swagger'
-require 'oj'
 require 'pry' if ENV['RACK_ENV'] == "development"
 
-require_relative 'models/procedure'
-
-Oj.mimic_JSON
+require_relative 'nhs_hack_day'
 
 module NHSHackDay
   class API < Grape::API
@@ -29,11 +26,49 @@ module NHSHackDay
         header "X-Reason", message
         body nil
       end
+
+      def procedure
+        if p = Procedure[params[:procedure_id]]
+          p.attributes
+        end
+      end
     end
 
     namespace :procedures do
-      get '/:id' do
-        not_found('procedure could not be found')
+      desc "Returns a single procedure"
+      params do
+        requires :procedure_id, :type => Integer, :desc => "Procedure identifier"
+      end
+      get '/:procedure_id' do
+        procedure or not_found('procedure could not be found')
+      end
+
+      desc "Creates a new procedure"
+      params do
+        requires :name, :type => String, :desc => "URL-friendly procedure name"
+        requires :title, :type => String, :desc => "Human friendly procedure name"
+      end
+      post '/' do
+        begin
+          Procedure.create(
+            :name  => params[:name],
+            :title => params[:title]
+          ).attributes
+        rescue Ohm::UniqueIndexViolation
+          already_exists('procedure already exists')
+        end
+      end
+    end
+
+    namespace :search do
+      desc "Searches across all procedure names"
+      params do
+        requires :term, :type => String, :desc => "Search term"
+      end
+      get '/:term' do
+        Procedure.all.select { |p|
+          p.name.index(/#{params[:term]}/i)
+        }.map(&:attributes)
       end
     end
   end
--- test/api_test.rb
+++ test/api_test.rb
@@ -3,6 +3,24 @@ require 'rack/test'
 
 require 'api'
 
+def lumbar_puncture
+  {
+    :name  => "lumbar-puncture",
+    :title => "Lumbar Puncture"
+  }
+end
+
+def arterial_black_guess_sampling
+  {
+    :name  => "arterial-black-guess-sampling",
+    :title => "Arterial Black Guess Sampling"
+  }
+end
+
+def new_procedure(hash)
+  Procedure.create(hash)
+end
+
 module NHSHackDay
   describe API do
     include Rack::Test::Methods
@@ -12,16 +30,65 @@ module NHSHackDay
     end
 
     def body
-      Oj.load(last_response.body)
+      MultiJson.load(last_response.body, :symbolize_keys => true)
     end
 
-    describe "GET /proceudre/:id" do
+    before do
+      Procedure.conn.redis.flushdb
+    end
+
+    describe "GET /procedures/:id" do
       describe "when procedure does not exist" do
         it "returns 404" do
           get("/procedures/0")
           last_response.headers['X-Reason'].must_equal 'procedure could not be found'
           last_response.status.must_equal 404
         end
+      end
+
+      describe "when procedure exists" do
+        before do
+          @procedure = new_procedure(lumbar_puncture)
+        end
+
+        it "returns procedure as JSON" do
+          get("/procedures/#{@procedure.id}")
+          last_response.status.must_equal 200
+          body.must_equal @procedure.attributes
+        end
+      end
+    end
+
+    describe "POST /procedures" do
+      describe "when procedure does not exist" do
+        it "creates a new one" do
+          post("/procedures", lumbar_puncture)
+          last_response.status.must_equal 201
+        end
+      end
+
+      describe "when procedure exists" do
+        before do
+          new_procedure(lumbar_puncture)
+        end
+
+        it "won't create a new one" do
+          post("/procedures", lumbar_puncture)
+          last_response.headers['X-Reason'].must_equal 'procedure already exists'
+          last_response.status.must_equal 403
+        end
+      end
+    end
+
+    describe "GET /search" do
+      before do
+        @matching = new_procedure(lumbar_puncture)
+        @non_matching = new_procedure(arterial_black_guess_sampling)
+      end
+      it "returns procedures where name matches" do
+        get("/search/lumbar")
+        last_response.status.must_equal 200
+        body.map { |p| p[:title] }.must_equal [@matching.title]
       end
     end
   end
