66322be79a278f6df308a4f37db1ba0eed67c88c
Szymon Witamborski <szywon@live.com>
2013-01-27 01:15:54 +0000

    Improved UX of the search page.

--- public/search.js
+++ public/search.js
@@ -5,7 +5,10 @@ function Search ($scope, $resource) {
   var searcher = $resource('/search/:term.json');
 
   $scope.search = function () {
+    $scope.status  = 'querying';
+    $scope.results = null;
     searcher.query({ term: $scope.term }, function (results) {
+      $scope.status  = 'got_reply';
       $scope.results = results;
     });
   };
