b65c26c3efa8ff3f1a1aee67307b65ed8bc5d281
Szymon Witamborski <szywon@live.com>
2013-01-27 14:31:49 +0000

    Uploading new procedures (broken at this point).

--- public/new_procedure.js
+++ public/new_procedure.js
@@ -8,36 +8,63 @@ function NewProcedure ($scope, $resource, $routeParams) {
     list.push('');
   };
 
-  function get_values (title_class, text_class) {
-    var things = [];
+  function get_values (fields) {
+    var pairs = _.pairs(fields);
+    var names = _.pluck(pairs, 0);
+    var values = pairs.map(function (pair) {
+      var name     = pair[0];
+      var selector = pair[1];
+      var elements = document.querySelectorAll(selector);
+      return _.pluck(elements, 'value');
+    });
 
-    var titles = document.querySelectorAll(title_class);
-    var texts = document.querySelectorAll(text_class);
-
-    for (var i = 0; i < titles.length; ++i) {
-      things.push({
-        title: titles[i].value,
-        text:  texts[i].value
+    return _.zip.apply(_, values).map(function (value) {
+      var result = {};
+      value.map(function (value, i) {
+        result[names[i]] = value;
       });
-    }
-
-    return things;
+      return result;
+    });
   }
 
-  var saver = $resource('/procedures.json');
+  var saver      = $resource('/procedures.json');
+  var step_saver = $resource('/steps.json');
+
+  function save_steps (proc_id, steps) {
+    if (!steps.length) {
+      location.hash = '#/procedure/' + proc_id;
+      return;
+    }
+
+    var step = steps.shift();
+    $scope.message = 'Saving step: ' + (step.id+1) + ' ' + step.title;
+
+    _.extend(step, { procedure_id: proc_id });
+    step_saver.save(step, function success () {
+      save_steps(steps);
+    }, function error () {
+      $scope.message = 'Error saving step: ' + (step.id+1) + ' ' + step.title;
+      $scope.message_class = 'error';
+    });
+  }
 
   $scope.save = function () {
     $scope.message = "Submitting...";
     $scope.message_class = '';
-    var steps = get_values('.step_title', '.step_text');
-    var complications = get_values('.complication_title',
-      '.complication_text');
+    var steps = get_values({
+      title:  '.step_title',
+      to_do:  '.step_to_do',
+      to_say: '.step_to_say'
+    });
 
     saver.save({
-      title: $scope.title
+      title:       $scope.title,
+      consent:     $scope.consent,
+      materials:   $scope.materials,
+      precautions: $scope.precautions
     }, function success (answer) {
-      $scope.message = "Saved.";
-      location.hash = '#/procedure/' + answer.id;
+      $scope.message = "Saved procedure.";
+      save_steps(steps);
     }, function error () {
       $scope.message_class = 'error';
       $scope.message = "Coudn't submit your procedure. Try a different title.";
